28cf4bf4f6b37d3aaef77fed23e3141d1cd90eda
Serrano Jorge <jsp@spain.conseur.org>
2017-09-04 10:04:32 +0200

    Visual Studio 2017 and .NET Core 2.0 - Count Words sample

---
+++ CountWords/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CountWords
+{
+    using System;
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write a text:");
+            var text = Console.ReadLine();
+
+            var counter = new Counter();
+            Console.WriteLine(counter.WordsFromText(text).ToString());
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
